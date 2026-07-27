70abe152ebb470eb26e3f3077569b94643031862
Guilherme <guilhermehenrike0412@gmail.com>
2026-07-27 10:46:05 -0300

    base do projeto

---
+++ src/test/java/com/example/Ratefy/RatefyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Ratefy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatefyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
