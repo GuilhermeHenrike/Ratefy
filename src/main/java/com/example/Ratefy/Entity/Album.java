4942cb8acb8e9b162632b6181d25c59ff382cf36
Guilherme <guilhermehenrike0412@gmail.com>
2026-07-27 22:14:54 -0300

    feat: base das entidades de Album e Users

---
+++ src/main/java/com/example/Ratefy/Entity/Album.java
@@ -0,0 +1,31 @@
+package com.example.Ratefy.Entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "albums")
+public class Album {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String coverUrl;
+    private String title;
+    private String artist;
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    @JsonIgnore
+    private Users user;
+}
---
+++ src/main/java/com/example/Ratefy/Entity/Users.java
@@ -0,0 +1,28 @@
+package com.example.Ratefy.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "users")
+public class Users {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Album> albuns;
+}
