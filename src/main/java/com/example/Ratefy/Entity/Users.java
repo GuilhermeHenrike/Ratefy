b4ed7ca46dfc25a15a7f2ce9bdda46ddd817401d
Guilherme <guilhermehenrike0412@gmail.com>
2026-08-18 11:31:40 -0300

    feat: resolvendo o problema de contas duplicadas

--- src/main/java/com/example/Ratefy/Entity/Users.java
+++ src/main/java/com/example/Ratefy/Entity/Users.java
@@ -20,7 +20,9 @@ public class Users {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(unique = true, nullable = false)
     private String username;
+
     private String password;
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
